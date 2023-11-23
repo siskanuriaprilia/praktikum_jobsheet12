801e19b8b59d4f0786c9f51bb28948ebbb08f2f1
siskanuriaprilia <aprilias878@gmail.com>
2023-11-23 11:53:56 +0700

    Percobaan 3

--- Ucapan_27.java
+++ Ucapan_27.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
 public class Ucapan_27 {
-    
-    public static String PenerimaUcapan(){
+public static String PenerimaUcapan(){
         Scanner sc = new Scanner(System.in);
         System.out.println("Tuliskan NAMA orang yang ingin Anda beri ucapan: ");
         String nama0rang = sc.nextLine();
         sc.close();;
         return nama0rang;
     }  
+
+    public static void main(String[] args) {
+        String nama = PenerimaUcapan();
+        System.out.println("Thank you "+nama+"\nMay the force be with you");
+    }
 }
